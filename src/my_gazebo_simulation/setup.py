3a4bfeefa6c8c9fda0d3777a96353bb8447fd71d
root <root@LAPTOP-2NF80VST>
2024-11-19 03:26:41 -0500

    so tired

--- src/my_gazebo_simulation/setup.py
+++ src/my_gazebo_simulation/setup.py
@@ -6,16 +6,17 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    py_modules=[
-        'my_gazebo_simulation.my_gazebo_scripts.custom_turtlebot_env',
-        'my_gazebo_simulation.my_gazebo_scripts.train_trpo_turtlebot',
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='your_name',
     maintainer_email='your_email@example.com',
-    description='My Gazebo Simulation Package',
-    license='TODO',
+    description='Description of my_gazebo_simulation package',
+    license='License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [

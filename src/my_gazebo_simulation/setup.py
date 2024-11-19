a60ce2449b0eb27cf14909168395d53e1b599eec
root <root@LAPTOP-2NF80VST>
2024-11-19 02:52:56 -0500

    new update

--- src/my_gazebo_simulation/setup.py
+++ src/my_gazebo_simulation/setup.py
@@ -6,13 +6,16 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    py_modules=[],
+    py_modules=[
+        'my_gazebo_simulation.my_gazebo_scripts.custom_turtlebot_env',
+        'my_gazebo_simulation.my_gazebo_scripts.train_trpo_turtlebot',
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='your_name',
     maintainer_email='your_email@example.com',
-    description='A ROS 2 package for Gazebo simulations and RL training',
-    license='TODO: License declaration',
+    description='My Gazebo Simulation Package',
+    license='TODO',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [

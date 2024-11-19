ef88e34eef2912ce65c1796b4e5d4b7104c89720
root <root@LAPTOP-2NF80VST>
2024-11-19 14:35:31 -0500

    Back at this

--- src/my_gazebo_simulation/setup.py
+++ src/my_gazebo_simulation/setup.py
@@ -5,22 +5,17 @@ package_name = 'my_gazebo_simulation'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    packages=[package_name, package_name + '.my_gazebo_scripts'],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='your_name',
+    maintainer='Your Name',
     maintainer_email='your_email@example.com',
-    description='Description of my_gazebo_simulation package',
-    license='License declaration',
+    description='My Gazebo simulation package',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'train_trpo_turtlebot = my_gazebo_simulation.train_trpo_turtlebot:main',
+            'train_trpo_turtlebot = my_gazebo_simulation.my_gazebo_scripts.train_trpo_turtlebot:main',
         ],
     },
 )

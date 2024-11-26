d1bf5634b9bff8e5dfc97b139321ba098647dff8
root <root@LAPTOP-2NF80VST>
2024-11-26 14:14:35 -0500

    spawning turtlebot

--- src/my_gazebo_simulation/launch/my_custom_world.launch.py
+++ src/my_gazebo_simulation/launch/my_custom_world.launch.py
@@ -2,7 +2,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
@@ -15,9 +15,10 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     x_pose = LaunchConfiguration('x_pose', default='0.0')
     y_pose = LaunchConfiguration('y_pose', default='0.0')
+    turtlebot_model = LaunchConfiguration('turtlebot_model', default='burger')
     
-    world_file = os.path.join(my_gazebo_simulation_pkg, 'worlds', 'custom_world.world')
-    #world_file = os.path.join(my_gazebo_simulation_pkg, 'worlds', 'RLTestingENV.world')
+    #world_file = os.path.join(my_gazebo_simulation_pkg, 'worlds', 'custom_world.world')
+    world_file = os.path.join(my_gazebo_simulation_pkg, 'worlds', 'RLTestingENV.world')
     
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -37,7 +38,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(turtlebot3_gazebo_pkg, 'launch', 'robot_state_publisher.launch.py')
         ),
-        launch_arguments={'use_sim_time': use_sim_time}.items()
+        launch_arguments={'use_sim_time': use_sim_time, 'model': turtlebot_model}.items()
     )
     
     spawn_turtlebot_cmd = IncludeLaunchDescription(
@@ -46,11 +47,13 @@ def generate_launch_description():
         ),
         launch_arguments={
             'x_pose': x_pose,
-            'y_pose': y_pose
+            'y_pose': y_pose,
+            'model' : turtlebot_model
         }.items()
     )
     
     ld = LaunchDescription()
+    ld.add_action(DeclareLaunchArgument('turtlebot_model', default_value='burger'))
     ld.add_action(gzserver_cmd)
     ld.add_action(gzclient_cmd)
     ld.add_action(robot_state_publisher_cmd)
